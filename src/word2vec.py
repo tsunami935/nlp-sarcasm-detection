edb7e28d287508fd512069bb8e45fee58f76587f
bumbledbees <sam@bombus.cloud>
2024-12-08 16:39:56 -0500

    Add code to specify w2v training params
    - moved core training functionality out of main() in word2vec.py and
      into a new function, w2v_train().
    - ensured value of lockf in .intersect_word2vec_format() is being
      provided.
    - added ability to provide command-line arguments to word2vec.py
    - added a bash script to train embeddings with various lockf values.
    - added some extra entries to .gitignore

--- src/word2vec.py
+++ src/word2vec.py
@@ -1,10 +1,12 @@
+from argparse import ArgumentParser
 from collections.abc import Iterator
+from itertools import product
 import json
 from pathlib import Path
 import sys
 
 import numpy as np
-from gensim.models import KeyedVectors, Word2Vec
+from gensim.models import Word2Vec
 
 from data_normalize import normalize_sentence
 
@@ -19,22 +21,35 @@ class Sentences:
                 data = json.loads(entry)
                 yield normalize_sentence(data['headline'])
 
-def main() -> int:
+
+def w2v_train(
+    vector_size: int = 300,
+    epochs: int = 50,
+    window: int = 5,
+    min_count: int = 1,
+    lockf: float = 0.0,
+) -> None:
     DATA_DIR = Path(__file__).parent.parent / 'data'
     GOOGLE_WORD2VEC = DATA_DIR / 'GoogleNews-vectors-negative300.bin'
     DATASET_JSON = DATA_DIR / 'Sarcasm_Headlines_Dataset.json'
-    OUTPUT_WORD2VEC = DATA_DIR / 'w2v-headline-embeddings-300d.bin'
+    OUTPUT_WORD2VEC = \
+        DATA_DIR / f'w2v-headline-embeddings-300d-e{epochs}-lockf{lockf}.bin'
+
+    print(f'-- w2v_train({vector_size=}, {window=}, {min_count=}, {epochs=}, '
+          f'{lockf=}) --')
 
     print('Initializing model...')
     headlines = Sentences(DATASET_JSON)
-    model = Word2Vec(vector_size=300, window=5, min_count=1)
+    model = Word2Vec(vector_size=vector_size, window=window, epochs=epochs,
+                     min_count=min_count)
 
     print('Building vocabulary...')
     model.build_vocab(headlines)
 
     print('Loading pretrained word2vec embeddings...')
-    model.wv.vectors_lockf = np.ones(len(model.wv))
-    model.wv.intersect_word2vec_format(GOOGLE_WORD2VEC, binary=True)
+    model.wv.vectors_lockf = np.ones((len(model.wv),), dtype=np.float32)
+    model.wv.intersect_word2vec_format(GOOGLE_WORD2VEC, binary=True,
+                                       lockf=lockf)
 
     print('Learning embeddings...')
     model.train(headlines,
@@ -44,7 +59,28 @@ def main() -> int:
     print(f'Saving embeddings to {OUTPUT_WORD2VEC}...')
     model.wv.save(str(OUTPUT_WORD2VEC))
 
+
+def main() -> int:
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--min-count', type=int)
+    parser.add_argument('-w', '--window', type=int)
+    parser.add_argument('-e', '--epochs', type=int)
+    parser.add_argument('-l', '--lockf', type=float)
+    args = parser.parse_args()
+
+    # this is kinda dumb but it works!
+    kwargs = {k: v for k, v in args._get_kwargs() if v is not None}
+    if kwargs:
+        w2v_train(**kwargs)
+        return 0
+
+    # if no arguments were passed...
+    epochs = (5, 50, 100, 200)
+    lockfs = (0.0, 1.0)
+    for epoch, lockf in product(epochs, lockfs):
+        w2v_train(epochs=epoch, lockf=lockf)
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
