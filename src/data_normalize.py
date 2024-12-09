5199564a21ed584ff60dc644098fe34dc2a41776
Nam <ndbui03@gmail.com>
2024-12-08 22:36:28 -0500

    Fixed tokenization
    Vocabulary, word2ind, and embeddings will need to be rebuilt

--- src/data_normalize.py
+++ src/data_normalize.py
@@ -13,6 +13,42 @@ except LookupError:
     nltk.download("punkt")
 
 
+def tokenize_single_quote(tokens: list[str]) -> list[str]:
+    size = len(tokens)
+    start = None
+    end = None
+    i = 0
+    while i < size:
+        if tokens[i].startswith("'"):
+            start = i
+        if tokens[i].endswith("'") and start is not None:
+            end = i
+            prev = tokens[:start]
+            curr = (
+                ["'", tokens[start][1:]]
+                + tokens[start + 1 : end]
+                + [tokens[end][:-1], "'"]
+                if start != end
+                else ["'", tokens[i][1:-1], "'"]
+            )
+            next = tokenize_single_quote(tokens[i + 1 :]) if i + 1 < size else []
+            return prev + curr + next
+        if tokens[i].endswith(("':", "',")) and start is not None:
+            end = i
+            prev = tokens[:start]
+            curr = (
+                ["'", tokens[start][1:]]
+                + tokens[start + 1 : end]
+                + [tokens[end][:-2], "'", tokens[end][-1]]
+                if start != end
+                else ["'", tokens[i][1:-2], "'", tokens[end][-1]]
+            )
+            next = tokenize_single_quote(tokens[i + 1 :]) if i + 1 < size else []
+            return prev + curr + next
+        i += 1
+    return tokens
+
+
 def tokenize_hyphen(tokens: list[str]) -> list[str]:
     hyphen = " " + TK_HYPH + " "
     size = len(tokens)
@@ -32,7 +68,8 @@ def normalize_sentence(sentence: str) -> str:
     # Turn all lowercase
     # Turn contractions to canonical form
     # Tokenize sentences
-    tks = tokenize_hyphen(word_tokenize(contractions.fix(sentence.lower())))
+    tks = tokenize_single_quote(sentence.lower().split(" "))
+    tks = tokenize_hyphen(word_tokenize(contractions.fix(" ".join(tks))))
     # Add start and end tokens
     return [TK_START] + tks + [TK_END]
 
