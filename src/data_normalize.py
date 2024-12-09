953856fb592332956133d66f00abccc0f094e9be
bumbledbees <sam@bombus.cloud>
2024-12-08 21:44:56 -0500

    fixes in data_normalize.py
    - added tokenize_single_quote() function to properly tokenize single
      quotes in output of word_tokenize().
    - added tokenize_edge_cases() function to fix a few miscallaneous
      oddities.
    - added some test cases, along with a function to manually search for
      bad apostrophe/single quote tokenization in test_data_normalize.py.
    - fixed a few misformatted headlines in Sarcasm_Headlines_Dataset.json.
    - reorganized imports.

--- src/data_normalize.py
+++ src/data_normalize.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
-import pandas as pd
-from common import *
+from collections.abc import Iterable
+from itertools import chain
+import sys
+
 
 import contractions
-
 import nltk
 from nltk.tokenize import word_tokenize
+import pandas as pd
+
+from common import TK_END, TK_HYPH, TK_START
 
 try:
     nltk.find("tokenizers/punkt/english.pickle")
@@ -28,13 +32,78 @@ def tokenize_hyphen(tokens: list[str]) -> list[str]:
     return tokens
 
 
+def tokenize_edge_cases(tokens: list[str]) -> Iterable[str]:
+    iter_tokens = enumerate(tokens)
+    n_tokens = len(tokens)
+
+    for idx, token in iter_tokens:
+        # undo mangling by contractions library
+        if token == "you.s":
+            token = "u.s"
+            if idx < (n_tokens - 1) and tokens[idx + 1] == ".":
+                _ = next(iter_tokens)  # advance the iterator to skip the "."
+                token += "."
+        # sometimes "'n" is written "'n'". try to fix this.
+        elif token == "'n" and idx < (n_tokens - 1) and tokens[idx + 1] == "'":
+            _ = next(iter_tokens)  # advance the iterator to skip the "'"
+            token += "'"
+        yield token
+
+
+
+SQ_WORDS = ("'round", "'til", "'tis", "'n", "'n'")
+SQ_SUFFIXES = ("'s", "'nt", "'ve", "'d", "''")
+
+
+def tokenize_single_quote(tokens: list[str]) -> Iterable[str]:
+    lquote_stack: list[int] = []
+    quote_indices: list[int] = []
+    for idx, token in enumerate(tokens):
+        if "'" not in token or token in chain(SQ_WORDS, SQ_SUFFIXES):
+            continue
+
+        # assume these are right-quotes
+        if token == "'" or token.endswith("'"):
+            if lquote_stack:
+                quote_indices.append(lquote_stack.pop())
+                quote_indices.append(idx)
+            # assume rquote not following an lquote is actually an lquote
+            else:
+                lquote_stack.append(idx)
+        elif token.startswith("'"):
+            lquote_stack.append(idx)
+
+    # if lquote_stack:
+    #     print(f"warning: sentence {tokens} might have unbalanced quotes!",
+    #           file=sys.stderr)
+    for idx, token in enumerate(tokens):
+        if token in ("'", "''"):
+            yield token
+            continue
+        if token.startswith("'") and token.endswith("'"):
+            for t in "'", token[1:-1], "'":
+                yield t
+            continue
+        if idx in quote_indices:
+            if token.startswith("'"):
+                for t in "'", token[1:]:
+                    yield t
+            elif token.endswith("'"):
+                for t in token[:-1], "'":
+                    yield t
+            continue
+        yield token
+
+
 def normalize_sentence(sentence: str) -> list[str]:
     # Turn all lowercase
     # Turn contractions to canonical form
     # Tokenize sentences
-    tks = tokenize_hyphen(word_tokenize(contractions.fix(sentence.lower())))
+    tks = tokenize_hyphen(word_tokenize(contractions.fix(sentence).lower()))
+    tks = tokenize_edge_cases(list(tokenize_single_quote(tks)))
+
     # Add start and end tokens
-    return [TK_START] + tks + [TK_END]
+    return [TK_START, *tks, TK_END]
 
 
 if __name__ == "__main__":
